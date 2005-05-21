f13b76159099c88c1d19b8433e47f5a7e2fac516
nigelj <nigelj@3ab234d3-4ee5-0310-bf13-923e1ee4b75c>
2005-05-21 20:30:57 +0000

    Last commit didn't work... I think I found the problem though...

--- website/trunk/includes/footer.php
+++ website/trunk/includes/footer.php
@@ -60,7 +60,7 @@ GPL Document can also be found in the same directory as this file as "LICENSE"
   <!--<img src="" width="44" height="22">--> <a href="#">About Us</a> | <a href="#">Site
   Map</a> | <a href="#">Privacy Policy</a> | <a href="http://developer.berlios.de/project/memberlist.php?group_id=2320">Contact Us</a> | &copy;2004
   The IRC Bot Project (Brian Pankey & Nigel Jones).  All Rights Reserved<br>
-  Page Last Updated: <?php echo $version ?>
+  Page Last Updated: <?php echo $version; ?>
 </div> 
 <br> 
 </body>
--- website/trunk/svn.php
+++ website/trunk/svn.php
@@ -4,7 +4,7 @@
 
 */
 
-$version = "$Id$"
+$version = "$Id$";
 
 /*
 
